5ec5e6de044213b6586ad69cdbc75c4137c00dcc
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2026-02-01 16:43:29 +0600

    add

---
+++ Feb1/src/feb1/Feb1.java
@@ -0,0 +1,43 @@
+
+package feb1;
+
+import java.util.Scanner;
+
+public class Feb1 {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        int input = s.nextInt();
+        int res= CheackFactorial(input);
+        System.out.println(res);
+//int s = sum();
+//        System.out.println(s);
+        
+    }
+    
+//    public static int sum(){
+//    int n1= 30;
+//    int n2= 30;
+//    
+//    int sum = n1 + n2;
+//    
+//    return sum;
+//    }
+    public static int CheackFactorial(int num) {
+        int count=1;
+     for(int i= 1; i<=num; i++){
+     count = count*i;
+     
+     }
+//     int i = 1;
+//     while(i<=num){
+//           i++;
+//     count =count *i;
+//   
+//     }
+    
+             
+    return count;
+    }
+}
---
+++ Feb1/src/feb1/test.java
@@ -0,0 +1,13 @@
+
+package feb1;
+
+public class test {
+    public static void main(String[] args) {
+        int count=1;
+         for(int i= 1; i<=5; i++){
+     count = count*i;
+     }
+         System.out.println(count);
+    }
+    
+}
