954b1f59067d7c3cf112e91ed5b9c7b22e4e6618
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2026-02-28 16:30:47 +0600

    add

--- HomePrac/src/homeprac/HomePrac.java
+++ HomePrac/src/homeprac/HomePrac.java
@@ -1,7 +1,12 @@
 package homeprac;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HomePrac {
 
@@ -130,6 +135,31 @@ public class HomePrac {
 //            totalSum +=sum;
 //            num1=num2;
 //            num2=sum;
+//        }
+
+
+
+//PW
+//File f = new File("C:\\Users\\Admin\\Desktop\\jc.txt");
+//        
+//        try {
+//            try (PrintWriter pw = new PrintWriter(f)) {
+//                pw.println("Test");
+//                            for(int i = 0; i<=10; i++){
+//            pw.println(i + " JEE");
+//            } 
+//            }
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(PWio.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        
+//        try {
+//            Scanner s = new Scanner(f);
+//           while(s.hasNext()){
+//               System.out.println(s.nextLine());
+//           }
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(PWio.class.getName()).log(Level.SEVERE, null, ex);
 //        }
     }
 
