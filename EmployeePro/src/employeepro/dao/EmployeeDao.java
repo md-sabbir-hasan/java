3008deeb4916d1e67ee235f79006d03dd4035677
MD. SABBIR HASAN <sabbirhasan.stat@gmail.com>
2026-03-07 14:18:32 +0600

    add

--- EmployeePro/src/employeepro/dao/EmployeeDao.java
+++ EmployeePro/src/employeepro/dao/EmployeeDao.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package employeepro.dao;
 
-/**
- *
- * @author Admin
- */
+import employeepro.model.EmpData;
+import employeepro.util.DbUtil;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class EmployeeDao {
     
+    
+    DbUtil util = new DbUtil();
+    PreparedStatement ps;
+    ResultSet rs;
+    String sql = null;
+    public void saveUser(EmpData ed){
+        sql = "insert into user(name, salary, email) values(?,?,?)";
+        try {
+          
+            ps = util.getCon().prepareStatement(sql);
+            ps.setString(1, ed.getName());
+            ps.setDouble(2, ed.getSalary());
+            ps.setString(3, ed.getEmail());
+             
+            ps.close();
+            util.getCon().close();
+           
+
+
+            JOptionPane.showMessageDialog(null, "Data Saved");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Data not Save");
+            Logger.getLogger(EmployeeDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    public List<EmpData> findAllEmp(){
+    sql = "select * from employee";
+    
+    List<EmpData> eList= new ArrayList<>();
+    return eList;
+    }
 }
--- EmployeePro/src/employeepro/model/EmpData.java
+++ EmployeePro/src/employeepro/model/EmpData.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package employeepro.model;
 
-/**
- *
- * @author Admin
- */
 public class EmpData {
+    private int id;
+    private String name;
+    private double salary;
+    private String email;
+
+    public EmpData() {
+    }
+
+    public EmpData(int id, String name, double salary, String email) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.email = email;
+    }
+
+    public EmpData(String name, double salary, String email) {
+        this.name = name;
+        this.salary = salary;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    
+    
     
 }
